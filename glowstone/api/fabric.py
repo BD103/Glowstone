8a5e5a18797973b9d5a8ba8554dd8088e2e3eaac
BD103 <59022059+BD103@users.noreply.github.com>
2021-08-01 21:44:20 -0400

    feat: add get_fabric_versions func

--- glowstone/api/fabric.py
+++ glowstone/api/fabric.py
@@ -1,11 +1,24 @@
 import requests
 
+meta_url = "https://meta.fabricmc.net"
 
 def get_mc_versions(stable=True):
-    req = requests.get("https://meta.fabricmc.net/v2/versions/game")
+    req = requests.get(meta_url + "/v2/versions/game")
     res = []
 
     # Adds only stable releases if true, else adds everything
+    for i in req.json():
+        if stable and i["stable"]:
+            res.append(i)
+        elif not stable:
+            res.append(i)
+
+    return res
+
+def get_fabric_versions(stable=False):
+    req = requests.get(meta_url + "/v2/versions/loader")
+    res = []
+
     for i in req.json():
         if stable and i["stable"]:
             res.append(i)
